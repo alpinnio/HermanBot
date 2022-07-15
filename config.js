997ad5f79555e0eb2fb8650b13cf5160ba618b16
alpinnio <107537249+alpinnio@users.noreply.github.com>
2022-07-15 15:52:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 //────────────────────────────────────────────────────────────────────────────────────────                                                                                                                                                                                                                                                                */
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Kt69lMVe90WLBkCFcoYIyA']
+global.linkGC = ['https://chat.whatsapp.com/GYq4ymAN47h25srItm6ZgT']
 global.channelYT = ['https://youtube.com/channel/UCPhU6uA_3gUaEvyrFZFgJqg'] 
-global.owner = ['6283146208804', '62882008097970', '6283125926877']// Put your number here
-global.mods = [ '6283146208804' ,  '62882008097970' ,  '6283146208804' ] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.owner = ['6288220072756', '6288210072756', '6288210072756']// Put your number here
+global.mods = ['6288210072756' ,  '6288210072756' ,  '6288210072756' ] // Want some help?
+global.prems = ['6288210072756'] // Premium user has unlimited limit)
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -33,7 +33,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Herman'
+global.packname = 'Luccy'
 global.author = 'Bot'
 
 global.multiplier = 72366367263636 // The higher, The harder levelup
